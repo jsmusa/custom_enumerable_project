28da7980086086bc756e8ca34c9eebcc4bc867d0
Jojo <jonathansardeamusa@gmail.com>
2022-07-21 02:23:08 +0800

    Finish upto my_none? method

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,5 +1,60 @@
 module Enumerable
   # Your code goes here
+  def my_each_with_index
+    array = self
+
+    array.length.times do |num|
+      yield array[num], num
+    end
+
+    self
+  end
+
+  def my_select
+    array = []
+
+    self.length.times do |num|
+      if yield self[num]
+        array.push self[num]
+      end
+    end
+
+    array
+  end
+
+  def my_all?
+    self.length.times do |num|
+      unless yield self[num]
+        return false
+      end
+    end
+
+    true
+  end
+
+  def my_any?
+    self.length.times do |num|
+      if yield self[num]
+        return true
+      end
+    end
+
+    false
+  end
+
+  def my_none?
+    self.length.times do |num|
+      if yield self[num]
+        return false
+      end
+    end
+
+    true
+  end
+
+  # def my_count
+  #   self
+  # end
 end
 
 # You will first have to define my_each
@@ -8,4 +63,13 @@ end
 # to this method
 class Array
   # Define my_each here
+  def my_each
+    array = self
+
+    array.length.times do |num|
+      yield array[num]
+    end
+
+    self
+  end
 end
