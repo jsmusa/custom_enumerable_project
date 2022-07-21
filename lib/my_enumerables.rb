5b6cd58186488b9e0106ed9702fdcc043933a2d0
Jojo <jonathansardeamusa@gmail.com>
2022-07-21 18:16:51 +0800

    Finish upto my_inject method

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -52,9 +52,42 @@ module Enumerable
     true
   end
 
-  # def my_count
-  #   self
-  # end
+  def my_count
+    count = 0
+
+    if block_given?
+      self.length.times do |num|
+        if yield self[num]
+          count += 1
+        end
+      end
+
+      count
+    else
+      self.length
+    end
+  end
+
+  def my_map
+    array = []
+
+    self.length.times do |num|
+      new = yield self[num]
+      array.push new
+    end
+
+    array
+  end
+
+  def my_inject(initial = self[0])
+    initial
+
+    self.length.times do |num|
+      initial = yield initial, self[num]
+    end
+
+    initial
+  end
 end
 
 # You will first have to define my_each
